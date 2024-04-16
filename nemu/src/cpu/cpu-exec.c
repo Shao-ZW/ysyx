ba97453689f8bdbc4d04d0e9cd83936570ac187f
tracer-ysyx <tracer@ysyx.org>
2024-04-16 20:48:30 +0800

    > compile NEMU ysyx_22040000 邵卓炜 Linux shao-VirtualBox 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:48:30 up  3:11,  1 user,  load average: 0.40, 0.22, 0.19

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -52,7 +52,6 @@ static void exec_once(Decode *s, vaddr_t pc) {
   s->snpc = pc;
   isa_exec_once(s);
   cpu.pc = s->dnpc;
-  printf("%x %x\n", pc, cpu.pc);
 #ifdef CONFIG_ITRACE
   char *p = s->logbuf;
   p += snprintf(p, sizeof(s->logbuf), FMT_WORD ":", s->pc);
