975e5c1bf39100f8acc8ce7c1c290bb876bc0493
tracer-ysyx <tracer@ysyx.org>
2024-04-19 14:40:54 +0800

    > compile NEMU ysyx_22040000 邵卓炜 Linux shao-VirtualBox 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:40:54 up 1 day,  2:38,  1 user,  load average: 0.41, 0.26, 0.21

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -70,61 +70,66 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 //   return oi;
 // }
 int sprintf(char *out, const char *fmt, ...) {
-  va_list args;
-  int fi, oi;
-  char c, *s;
-  char buf[12];
+    va_list args;
+    va_start(args, fmt);
 
-  va_start(args, fmt);
+    int oi = 0;
 
-  for(fi = 0, oi = 0; (c = fmt[fi]) != '\0'; fi++) {
-    if(c != '%') {
-      out[oi++] = c;
-    }
-    else {
-      switch((c = fmt[++fi])) {
-        case 'd': {
-          int arg = va_arg(args, int);
-          int t = 0;
-
-          if (arg < 0) {
-            out[oi++] = '-';
-            arg = -arg;
-          }
-          while(arg) {
-            buf[t++] = arg % 10;
-            arg /= 10;
-          }
-          while(t--) {
-            out[oi++] = buf[t];
-          }
-          break;
+    while (*fmt != '\0') {
+        if (*fmt != '%') {
+            out[oi++] = *fmt;
+            fmt++;
+            continue;
         }
-        case 's': {
-          if((s = va_arg(args, char*)) == 0)
-            s = "(null)";
-          while(*s) {
-            out[oi++] = *s;
-            s++;
-          }
-          break;
-        }
-        case '%':
-          out[oi++] = '%';
-          break;
-        default:
-          out[oi++] = '%';
-          out[oi++] = c;
-          break;
-      }
-    }
-  }
 
-  va_end(args);
-  out[oi] = '\0';
-  
-  return oi;
+        fmt++;
+
+        switch (*fmt) {
+            case 'd': {
+                int arg = va_arg(args, int);
+                int t = 0;
+                char buf[20]; // 可能的整数最大位数
+                if (arg < 0) {
+                    out[oi++] = '-';
+                    arg = -arg;
+                }
+                do {
+                    buf[t++] = arg % 10 + '0';
+                    arg /= 10;
+                } while (arg != 0);
+                while (t > 0) {
+                    out[oi++] = buf[--t];
+                }
+                break;
+            }
+            case 's': {
+                const char *s = va_arg(args, const char*);
+                if (!s) {
+                    s = "(null)";
+                }
+                while (*s != '\0') {
+                    out[oi++] = *s++;
+                }
+                break;
+            }
+            case '%':
+                out[oi++] = '%';
+                break;
+            default:
+                out[oi++] = '%';
+                out[oi++] = *fmt;
+                break;
+        }
+        fmt++;
+    }
+
+    va_end(args);
+
+    out[oi] = '\0'; // 添加字符串结束符
+
+    return oi;
 }
+
 int snprintf(char *out, size_t n, const char *fmt, ...) {
   panic("Not implemented");
 }
