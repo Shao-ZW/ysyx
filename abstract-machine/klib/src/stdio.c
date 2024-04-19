bd63f8a6f88229d94d898ee8ddfeb32cadda8d4b
tracer-ysyx <tracer@ysyx.org>
2024-04-19 14:15:02 +0800

    > compile NEMU ysyx_22040000 邵卓炜 Linux shao-VirtualBox 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:15:02 up 1 day,  2:12,  1 user,  load average: 0.18, 0.21, 0.25

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -17,7 +17,49 @@ int sprintf(char *out, const char *fmt, ...) {
   va_list args;
   va_start(args, fmt);
 
-  return 0;
+  int fi, oi;
+  char c, *s;
+  char buf[10];
+
+  for(fi = 0, oi = 0; fmt[fi] != '\0'; ++fi) {
+    c = fmt[fi];
+
+    if(c != '%') {
+      out[oi++] = c;
+    }
+    else {
+      switch((c = fmt[++fi])) {
+        case 'd':
+          int arg = va_arg(args, int);
+          int t = 0;
+          while(arg) {
+            buf[t++] = arg % 10;
+            arg /= 10;
+          }
+          while(t--) {
+            out[oi++] = buf[t];;
+          }
+          break;
+        case 's':
+          if((s = va_arg(args, char*)) == 0)
+            s = "(null)";
+          while(*s) {
+            out[oi++] = *s;
+            s++;
+          }
+          break;
+        case '%':
+          out[oi++] = '%';
+          break;
+        default:
+          out[oi++] = '%';
+          out[oi++] = c;
+          break;
+      }
+    }
+  }
+
+  return oi;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
