f2a021e55f56d475ac1973a357f75f16129aa72f
tracer-ysyx <tracer@ysyx.org>
2024-04-19 14:39:47 +0800

    > compile NEMU ysyx_22040000 邵卓炜 Linux shao-VirtualBox 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:39:47 up 1 day,  2:36,  1 user,  load average: 0.26, 0.18, 0.18

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -15,32 +15,36 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 
 int sprintf(char *out, const char *fmt, ...) {
   va_list args;
-  va_start(args, fmt);
-
   int fi, oi;
   char c, *s;
-  char buf[10];
+  char buf[12];
 
-  for(fi = 0, oi = 0; fmt[fi] != '\0'; ++fi) {
-    c = fmt[fi];
+  va_start(args, fmt);
 
+  for(fi = 0, oi = 0; (c = fmt[fi]) != '\0'; fi++) {
     if(c != '%') {
       out[oi++] = c;
     }
     else {
       switch((c = fmt[++fi])) {
-        case 'd':
+        case 'd': {
           int arg = va_arg(args, int);
           int t = 0;
+
+          if (arg < 0) {
+            out[oi++] = '-';
+            arg = -arg;
+          }
           while(arg) {
             buf[t++] = arg % 10;
             arg /= 10;
           }
           while(t--) {
-            out[oi++] = buf[t];;
+            out[oi++] = buf[t];
           }
           break;
-        case 's':
+        }
+        case 's': {
           if((s = va_arg(args, char*)) == 0)
             s = "(null)";
           while(*s) {
@@ -48,6 +52,7 @@ int sprintf(char *out, const char *fmt, ...) {
             s++;
           }
           break;
+        }
         case '%':
           out[oi++] = '%';
           break;
@@ -59,6 +64,9 @@ int sprintf(char *out, const char *fmt, ...) {
     }
   }
 
+  va_end(args);
+  out[oi] = '\0';
+  
   return oi;
 }
 
