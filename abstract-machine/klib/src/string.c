fb9e1a2af93349eb5fa8b7fda9127a5140e9a184
tracer-ysyx <tracer@ysyx.org>
2024-05-02 16:39:01 +0800

    > compile NEMU ysyx_23060289 邵卓炜 Linux shao-VirtualBox 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:39:01 up 2 days,  1:05,  0 users,  load average: 0.75, 0.45, 0.37

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,43 +5,106 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  const char *p = s;
+  while(*p)
+    p++;
+  return p - s;
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  char *p = dst;
+  while(*src)
+    *dst++ = *src++;
+  *dst = '\0';
+  return p;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  char *p = dst;
+  size_t i = 0;
+  while(i < n && *src) {
+    *dst++ = *src++;
+    i++;
+  }
+  *dst = '\0';
+  return p;
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  char *p = dst;
+  while(*dst)
+    dst++;
+  while(*src)
+    *dst++ = *src++;
+  *dst = '\0';
+  return p;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  while(*s1 && *s1 == *s2) {
+    s1++;
+    s2++;
+  }
+  return *s1 - *s2;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+  while(n && *s1 && *s1 == *s2) {
+    s1++;
+    s2++;
+    n--;
+  }
+  if(n == 0)
+    return 0;
+  return *s1 - *s2;
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  char *d = (char *)s;
+  for(int i = 0; i < n; ++i)
+    d[i] = c;
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
+  char *d = (char *)dst;
+  char *s = (char *)src;
+
+  if(d < s) {
+    while(n--)
+      *d++ = *s++;
+  }
+  else {
+    d += n;
+    s += n;
+    while(n--)
+      *--d = *--s;
+  }
+
+  return dst;
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+  return memmove(out, in, n);
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  unsigned char *p1 = (unsigned char *)s1;
+  unsigned char *p2 = (unsigned char *)s2;
+
+  while(n--) {
+    if(*p1 != *p2)
+      return *p1 - *p2;
+    p1++;
+    p2++;
+  }
+  return 0;
 }
 
 #endif
+
+
+
+
+
+
