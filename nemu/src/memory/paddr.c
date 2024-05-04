b093723490dad4cca813ddef5f53ace30c2e4445
tracer-ysyx <tracer@ysyx.org>
2024-05-04 19:18:43 +0800

    > compile NEMU ysyx_23060289 邵卓炜 Linux shao-VirtualBox 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:18:43 up  1:42,  0 users,  load average: 0.48, 0.23, 0.15

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -52,7 +52,9 @@ void init_mem() {
 
 word_t paddr_read(paddr_t addr, int len) {
   #ifdef CONFIG_MTRACE
+  printf("memory read:");
   printf(FMT_WORD, addr);
+  printf("\n");
   #endif
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
