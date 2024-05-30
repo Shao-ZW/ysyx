589e686820081a311122bfd5bd0c109b27ebed53
tracer-ysyx <tracer@ysyx.org>
2024-05-30 22:45:23 +0800

    > compile NEMU ysyx_23060289 邵卓炜 Linux shao-VirtualBox 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:45:23 up  8:03,  0 users,  load average: 0.75, 0.61, 0.47

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -31,7 +31,7 @@ $(BINARY):: compile_git
 # override ARGS += --log=$(BUILD_DIR)/nemu-log.txt
 # endif
 # endif
-ARGS = 
+
 
 override ARGS ?= --log=$(BUILD_DIR)/nemu-log.txt
 override ARGS += $(ARGS_DIFF)
