a10355e2026296219dab343083ea74c31ac178e7
tracer-ysyx <tracer@ysyx.org>
2024-05-31 23:01:35 +0800

    > compile NEMU ysyx_23060289 邵卓炜 Linux shao-VirtualBox 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:01:35 up 1 day,  8:19,  0 users,  load average: 0.78, 0.59, 0.44

--- npc/csrc/pmem.cpp
+++ npc/csrc/pmem.cpp
@@ -25,3 +25,26 @@ extern "C" void pmem_write(int waddr, int wdata, char wmask) {
     
     memory[aligned_addr / 4] = current_data;
 }
+
+void load_img() {
+  if (img_file == NULL) {
+    Log("No image is given. Use the default build-in image.");
+    return 4096; // built-in image size
+  }
+
+  FILE *fp = fopen(img_file, "rb");
+  Assert(fp, "Can not open '%s'", img_file);
+
+  fseek(fp, 0, SEEK_END);
+  long size = ftell(fp);
+
+  Log("The image is %s, size = %ld", img_file, size);
+
+  fseek(fp, 0, SEEK_SET);
+  int ret = fread(guest_to_host(RESET_VECTOR), size, 1, fp);
+  assert(ret == 1);
+
+  fclose(fp);
+  return size;
+}
+
