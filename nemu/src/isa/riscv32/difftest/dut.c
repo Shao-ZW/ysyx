801e790cdf0a6f46e3d034b110eb2ab1b98a4519
tracer-ysyx <tracer@ysyx.org>
2024-05-31 11:00:52 +0800

    > compile NEMU ysyx_23060289 邵卓炜 Linux shao-VirtualBox 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:00:52 up 20:18,  0 users,  load average: 0.59, 0.44, 0.48

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -20,17 +20,22 @@
 #define NR_GPR MUXDEF(CONFIG_RVE, 16, 32)
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  // if(ref_r->pc != pc) {
-  //   printf("ref-pc  %x   dut-pc  %x\n", ref_r->pc, pc);
-  //   return false;
-  // }
-  printf("%x %x\n", ref_r->pc, pc);
+  bool flag = true;
+
+  if(ref_r->pc != pc)
+    flag = false; 
+
   for(int i = 0; i < NR_GPR; ++i)
     if(ref_r->gpr[check_reg_idx(i)] != gpr(i))
-      return false;
+      flag = false;
+  
+  if(!flag) {
+    printf("ref-pc: "FMT_PADDR"  dut-pc: "FMT_PADDR"\n", ref_r->pc, pc);
+    // for(int i = 0; i < NR_GPR; ++i)
+    //   printf("ref-pc: "FMT_PADDR"  dut-pc: "FMT_PADDR"\n", ref_r->pc, pc);
+  }
 
-
-  return true;
+  return flag;
 }
 
 void isa_difftest_attach() {
