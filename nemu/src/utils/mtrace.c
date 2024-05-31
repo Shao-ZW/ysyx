3d51ddfcf5f3031ca90bd060606ef2f3c4d10d27
tracer-ysyx <tracer@ysyx.org>
2024-05-31 13:17:08 +0800

    > compile NEMU ysyx_23060289 邵卓炜 Linux shao-VirtualBox 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:17:08 up 22:35,  0 users,  load average: 0.27, 0.42, 0.38

--- nemu/src/utils/mtrace.c
+++ nemu/src/utils/mtrace.c
@@ -2,7 +2,7 @@
 #include <common.h>
 
 void mtrace_read(paddr_t addr) {
-    printf("memory read  address :  "FMT_WORD"\n", addr);
+    printf("memory read  address : "FMT_WORD"\n", addr);
 }
 
 void mtrace_write(paddr_t addr) {
