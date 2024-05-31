abcfe5558a85f8a64e86e446fbe052cff5912e72
shao <779224180@qq.com>
2024-05-31 12:08:00 +0800

    implement differential testing

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -17,8 +17,23 @@
 #include <cpu/difftest.h>
 #include "../local-include/reg.h"
 
+#define NR_GPR MUXDEF(CONFIG_RVE, 16, 32)
+
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  bool flag = true;
+
+  if(ref_r->pc != cpu.pc) {
+    printf("\ndut-pc : "FMT_PADDR"  ref-pc : "FMT_PADDR"\n", cpu.pc, ref_r->pc);
+    flag = false; 
+  }
+  
+  for(int i = 0; i < NR_GPR; ++i)
+    if(ref_r->gpr[check_reg_idx(i)] != gpr(i)) {
+      printf("dut-%-3s: "FMT_WORD"  ref-%-3s: "FMT_WORD"\n", reg_name(i), gpr(i), reg_name(i), ref_r->gpr[i]);
+      flag = false;
+    }
+  
+  return flag;
 }
 
 void isa_difftest_attach() {
