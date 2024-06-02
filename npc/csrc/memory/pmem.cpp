32867a461b2d54cd46dfc365427e1c384a108bea
tracer-ysyx <tracer@ysyx.org>
2024-06-03 00:50:41 +0800

    > sim RTL ysyx_23060289 邵卓炜 Linux shao-VirtualBox 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:50:41 up  4:49,  1 user,  load average: 0.17, 0.24, 0.24

--- npc/csrc/memory/pmem.cpp
+++ npc/csrc/memory/pmem.cpp
@@ -1,23 +1,23 @@
 #include <cstdint>
 
 #define MEMORY_SIZE 4096
+#define paddr_t uint32_t
 
-uint32_t pmem[MEMORY_SIZE] = {
-  0x00000297,  // auipc t0,0
-  0x00028823,  // sb  zero,16(t0)
-  0x0102c503,  // lbu a0,16(t0)
-  0x00100073,  // ebreak (used as nemu_trap)
-  0xdeadbeef,  // some data 
+uint8_t pmem[MEMORY_SIZE] = {
+  0x00, 0x00, 0x02, 0x97,  // auipc t0,0
+  0x00, 0x02, 0x88, 0x23,  // sb  zero,16(t0)
+  0x01, 0x02, 0xc5, 0x03,  // lbu a0,16(t0)
+  0x00, 0x10, 0x00, 0x73,  // ebreak (used as nemu_trap)
+  0xde, 0xad, 0xbe, 0xef,  // some data 
 };  // built-in image
 
 #define CONFIG_MBASE 0x80000000
 
-uint32_t guest_to_host(uint32_t addr) { return addr - CONFIG_MBASE; }
+uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
 
 extern "C" uint32_t pmem_read(uint32_t raddr) {
   // 总是读取地址为`raddr & ~0x3u`的4字节返回
-  return 0xdeadbeef;
-  return pmem[guest_to_host((uint32_t)raddr) & ~0x3u];
+  return *(uint32_t*)guest_to_host(raddr & ~0x3u);
 }
 
 extern "C" void pmem_write(int waddr, int wdata, char wmask) {
@@ -25,16 +25,16 @@ extern "C" void pmem_write(int waddr, int wdata, char wmask) {
   // `wmask`中每比特表示`wdata`中1个字节的掩码,
   // 如`wmask = 0x3`代表只写入最低2个字节, 内存中的其它字节保持不变
 
-  int aligned_addr = guest_to_host(waddr) & ~0x3u;
-  int current_data = pmem[aligned_addr / 4];
+  // int aligned_addr = guest_to_host(waddr) & ~0x3u;
+  // int current_data = pmem[aligned_addr / 4];
 
-  for (int i = 0; i < 4; i++) {
-    if (wmask & (1 << i)) {
-      current_data = (current_data & ~(0xFF << (i * 8))) | (wdata & (0xFF << (i * 8)));
-    }
-  }
+  // for (int i = 0; i < 4; i++) {
+  //   if (wmask & (1 << i)) {
+  //     current_data = (current_data & ~(0xFF << (i * 8))) | (wdata & (0xFF << (i * 8)));
+  //   }
+  // }
   
-  pmem[aligned_addr / 4] = current_data;
+  // pmem[aligned_addr / 4] = current_data;
 }
 
 // void init_mem() {
