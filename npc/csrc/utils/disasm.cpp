9827e7770e9412502a0afc37ba966bb54dea4235
tracer-ysyx <tracer@ysyx.org>
2024-06-04 00:53:59 +0800

    > sim RTL ysyx_23060289 邵卓炜 Linux shao-VirtualBox 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:53:59 up 11:19,  1 user,  load average: 0.72, 0.57, 0.48

--- npc/csrc/utils/disasm.cpp
+++ npc/csrc/utils/disasm.cpp
@@ -97,6 +97,7 @@ extern "C" void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int
   MCInst inst;
   llvm::ArrayRef<uint8_t> arr(code, nbyte);
   uint64_t dummy_size = 0;
+  puts("ok");
   gDisassembler->getInstruction(inst, dummy_size, arr, pc, llvm::nulls());
   puts("ok");
   std::string s;
