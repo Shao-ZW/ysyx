e939710717bc6e176f32c6b0774aeedc80dadac6
shao <779224180@qq.com>
2024-06-05 23:27:41 +0800

    add Makefile scripts

---
+++ npc/scripts/native.mk
@@ -0,0 +1,23 @@
+-include $(NPC_HOME)/../Makefile
+include $(NPC_HOME)/scripts/build.mk
+
+include $(NPC_HOME)/tools/difftest.mk
+
+override ARGS ?= --log=$(BUILD_DIR)/npc-log.txt
+override ARGS += $(ARGS_DIFF)
+
+# Command to execute NPC
+IMG ?=
+NPC_SIM := $(BIN) $(ARGS) $(IMG)
+
+sim: $(BIN)
+	$(call git_commit, "sim RTL") # DO NOT REMOVE THIS LINE!!!
+	$(NPC_SIM)
+
+vcd: $(VCD_FILE)
+	gtkwave $(VCD_FILE)
+
+clean:
+	rm -rf $(BUILD_DIR)
+	
+.PHONY: sim vcd clean
