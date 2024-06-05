13b690397537efd4e15e0a274fc6db991b815b93
tracer-ysyx <tracer@ysyx.org>
2024-06-05 18:21:04 +0800

    > sim RTL ysyx_23060289 邵卓炜 Linux shao-VirtualBox 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:21:04 up  1:30,  0 users,  load average: 0.71, 0.58, 0.41

--- npc/csrc/trace/mtrace.cpp
+++ npc/csrc/trace/mtrace.cpp
@@ -1,11 +1,11 @@
 #include "common.h"
 
 void mtrace_read(paddr_t addr, uint32_t rdata) {
-    printf(ANSI_FMT("memory raddr : " FMT_PADDR "  read data : " 
+    printf(ANSI_FMT("memory raddr : " FMT_PADDR "  rdata : " 
             FMT_WORD "\n", ANSI_FG_YELLOW), addr, rdata);
 }
 
 void mtrace_write(paddr_t addr, uint32_t wdata, char wmask) {
-    printf(ANSI_FMT("memory waddr : " FMT_PADDR "  write data : " 
-            FMT_WORD "  write mask : " FMT_WORD "\n", ANSI_FG_YELLOW), addr, wdata, wmask);
+    printf(ANSI_FMT("memory waddr : " FMT_PADDR "  wdata : " 
+            FMT_WORD "  wmask : " FMT_WORD "\n", ANSI_FG_YELLOW), addr, wdata, wmask);
 }
