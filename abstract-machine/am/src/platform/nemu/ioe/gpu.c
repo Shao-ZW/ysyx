f487b4ae1a577915358195fc460ba0723581282c
shao <779224180@qq.com>
2024-06-11 00:41:42 +0800

    implement keyboard and vga parts in nemu and am

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -7,14 +7,33 @@ void __am_gpu_init() {
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+  int vdata = inl(VGACTL_ADDR);
+  int width = vdata >> 16;
+  int height = vdata & 0xFFFF;
+  int vmemsz = width * height * sizeof(uint32_t);
+
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
+    .width = width, .height = height,
+    .vmemsz = vmemsz
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  int x = ctl->x, y = ctl->y;
+  int w = ctl->w, h = ctl->h;
+  int width = inl(VGACTL_ADDR) >> 16;
+  uint32_t *pixels = (uint32_t*)ctl->pixels;
+
+  int px, py;
+  for(int j = 0; j < h; ++j) {
+    for(int i = 0; i < w; ++i) {
+      px = x + i;
+      py = y + j;
+      outl(FB_ADDR + (py * width + px) * sizeof(uint32_t), pixels[j * w + i]);
+    }
+  }
+
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
